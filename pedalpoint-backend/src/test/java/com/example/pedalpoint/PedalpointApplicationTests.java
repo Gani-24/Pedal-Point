0c0a3e1d473de1a72451a293c125a6dac444fc9e
Sai Ganesh K <92284218+Gani-24@users.noreply.github.com>
2024-11-26 21:26:27 +0530

    added boilerplate backend

---
+++ pedalpoint-backend/src/test/java/com/example/pedalpoint/PedalpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pedalpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedalpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
